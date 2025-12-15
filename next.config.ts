eb4d8352d2febe1a7b44f31c58c18b7c0b0daaff
valebearzotti <valebearzotti1@gmail.com>
2025-12-15 12:53:34 -0300

    fix next config remotePatterns key

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["files.stripe.com"],
+    remotePatterns: [{ hostname: "files.stripe.com" }],
   },
 };
 

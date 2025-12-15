1caa589a256373b3dd1038427118ef5c25549a82
valebearzotti <valebearzotti1@gmail.com>
2025-12-15 14:22:40 -0300

    upd base url

--- lib/base-url.ts
+++ lib/base-url.ts
@@ -1,11 +1,11 @@
 export function getBaseUrl(): string {
   if (process.env.VERCEL_ENV === "production") {
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://gpt-apps-monetization.vercel.app`;
   }
 
   if (process.env.NODE_ENV === "development") {
     return "http://localhost:3000";
   }
 
-  return `https://${process.env.VERCEL_URL}`;
+  return `https://gpt-apps-monetization.vercel.app`;
 }
